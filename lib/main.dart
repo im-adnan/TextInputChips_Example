d365bf41eeba9ea8c28ccf5d6f02459c2918dbb8
Syed Mohd Adnan <sayedmuhammadadnan@outlook.com>
2022-09-18 23:07:32 +0530

    Removed Junk

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,20 @@
 import 'package:chips/InputChipExample.dart';
 import 'package:flutter/material.dart';
-import 'filterchip.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Chips',
       theme: ThemeData(
-        //primaryColor: new Color(0xffe91e63),
-        //primaryColor: new Color(0xffffc107),
         primaryColor: const Color(0xff6200ee),
-        //primaryColor: new Color(0xffF2601F),
       ),
-      //home: InputChipDisplay(),//inputChip
-      //home: ChoiceChipDisplay(),//choicechip
-      home: const InputChipExample(), //filterchip
-      //home: ActionChipDisplay(),//actionchip
+      home: const InputChipExample(),
     );
   }
 }
